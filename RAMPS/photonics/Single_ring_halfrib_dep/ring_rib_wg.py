ab1164f6dba8c6bc63d5693e81ab04b6d94ff034
Xinchang Zhang <zhangxc@scc2.bu.edu>
2024-12-22 00:45:54 -0500

    Add M5 outer ring out side patch

--- RAMPS/photonics/Single_ring_halfrib_dep/ring_rib_wg.py
+++ RAMPS/photonics/Single_ring_halfrib_dep/ring_rib_wg.py
@@ -1262,9 +1262,9 @@ class RingRibWg_sr(BPG.PhotonicTemplateBase):
                     patch_width = 0.2
                     delta_theta_pacth = 0.05
                     num_of_patch = 25
+                    theta_patch = i * delta_theta
                     for j in range(num_of_patch):
                         k = j-num_of_patch//2
-                        theta_patch = i * delta_theta
                         centre_offset = 0.44
                         patch_centre = (np.cos(theta_patch+k*delta_theta_pacth) * r_via + self.ring_loc[0]+centre_offset, centre_offset+np.sin(theta_patch+k*delta_theta_pacth) * r_via + self.ring_loc[1])
                         self.add_rect(layer=('M5','drawing'),
@@ -1273,6 +1273,22 @@ class RingRibWg_sr(BPG.PhotonicTemplateBase):
                                     left=patch_centre[0]-0.5*patch_width,
                                     top=patch_centre[1]+0.5*patch_width,
                                     resolution=self.grid.resolution)
+                            
+                        )
+                    delta_theta_pacth_out = 0.035
+                    num_of_patch_out = 9
+                    for j in range(num_of_patch_out):
+                        k = j-num_of_patch_out//2
+                        theta_patch = i * delta_theta
+                        centre_offset = 0.44+1.45
+                        patch_centre = (np.cos(theta_patch+k*delta_theta_pacth_out) * (r_via+centre_offset) + self.ring_loc[0], np.sin(theta_patch+k*delta_theta_pacth_out) *(r_via+centre_offset) + self.ring_loc[1])
+                        self.add_rect(layer=('M5','drawing'),
+                            bbox=BBox(right=patch_centre[0]+0.5*patch_width,
+                                    bottom=patch_centre[1]-0.5*patch_width,
+                                    left=patch_centre[0]-0.5*patch_width,
+                                    top=patch_centre[1]+0.5*patch_width,
+                                    resolution=self.grid.resolution)
+                            
                         )
                 
                 if offset == 0.5:
